557530678d67842a2a48564672e04e4a2154b8e1
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-02 17:23:13 -0700

    Create __main__.py

---
+++ todo/__main__.py
@@ -0,0 +1,24 @@
+import sys
+
+import PySide6.QtQuick as _  # needed for PyInstaller
+from PySide6.QtGui import QFontDatabase, QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuickControls2 import QQuickStyle
+
+import todo.resources as _  # compiled resources
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    QQuickStyle.setStyle("Material")
+
+    QFontDatabase.addApplicationFont(":/fonts/Roboto-Regular.ttf")
+    QFontDatabase.addApplicationFont(":/fonts/Roboto-Medium.ttf")
+
+    engine.load("qrc:/qml/Todo.qml")
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec())
