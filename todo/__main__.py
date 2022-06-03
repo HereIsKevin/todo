a5929a9b0732e71b2820d99ebfecab81e81aa0b2
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-03 16:56:01 -0700

    Override graphics backend and API

--- todo/__main__.py
+++ todo/__main__.py
@@ -1,14 +1,26 @@
+import platform
 import sys
 
-import PySide6.QtQuick as _  # needed for PyInstaller
 from PySide6.QtGui import QFontDatabase, QGuiApplication, QIcon
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
 from PySide6.QtQuickControls2 import QQuickStyle
 
 import todo.resources as _  # compiled resources
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+
+    match platform.system():
+        case "Darwin":
+            graphics_api = QSGRendererInterface.Metal
+        case "Windows":
+            graphics_api = QSGRendererInterface.OpenGL  # Direct3D11 has issues
+        case _:
+            graphics_api = QSGRendererInterface.OpenGL
+
+    QQuickWindow.setSceneGraphBackend("rhi")
+    QQuickWindow.setGraphicsApi(graphics_api)
 
     app.setOrganizationName("HereIsKevin")
     app.setOrganizationDomain("hereiskevin.dev")
