50a6e05d0a89eb6e165a9e61a189a18380a812d8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-02 23:29:57 -0700

    Add icon into frozen bundle

--- scripts/freeze.py
+++ scripts/freeze.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+import platform
 from pathlib import Path
 
-import PyInstaller.__main__
+from PyInstaller.__main__ import run
 
 NAME = "Todo"
 BASE = Path(__file__).resolve().parent.parent
 MAIN = BASE / "todo" / "__main__.py"
+ICONS = BASE / "resources" / "icons" / "app"
 
 if __name__ == "__main__":
-    PyInstaller.__main__.run([str(MAIN), "--windowed", "--name", NAME])
+    if platform.system() == "Darwin":
+        icon = ICONS / "Todo.icns"
+    else:
+        icon = ICONS / "Todo.ico"
+
+    run([str(MAIN), "--windowed", "--name", NAME, "--icon", str(icon)])
