b14b90a49848495da90febf9260b7353d8c938cc
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-05 16:23:36 -0700

    Hide Dock icon and add version number for macOS

--- scripts/freeze.py
+++ scripts/freeze.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python3
 
 import platform
+import re
+import subprocess
 from pathlib import Path
 
-from PyInstaller.__main__ import run
-
 NAME = "Todo"
+VERSION = "0.1.0"
 BASE = Path(__file__).resolve().parent.parent
+SPEC = BASE / "Todo.spec"
 MAIN = BASE / "todo" / "__main__.py"
 ICONS = BASE / "resources" / "icons" / "app"
 
 if __name__ == "__main__":
-    if platform.system() == "Darwin":
+    darwin = platform.system() == "Darwin"
+
+    if darwin:
         icon = ICONS / "Todo.icns"
     else:
         icon = ICONS / "Todo.ico"
 
-    run([str(MAIN), "--windowed", "--name", NAME, "--icon", str(icon)])
+    spec_command = [
+        "pyi-makespec",
+        str(MAIN),
+        "--windowed",
+        "--name",
+        NAME,
+        "--icon",
+        str(icon),
+    ]
+
+    subprocess.run(spec_command)
+
+    if darwin:
+        pattern = re.compile(r"BUNDLE\((.*)\)", flags=re.DOTALL)
+        contents = SPEC.read_text()
+
+        info_plist = {"LSUIElement": True}
+        replacement = rf'BUNDLE(\1version="{VERSION}", info_plist={info_plist})'
+
+        SPEC.write_text(pattern.sub(replacement, contents))
+
+    subprocess.run(["pyinstaller", str(SPEC)])
