17806da383e6037eacb82fcc0b272ef55b224216
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-04 21:05:09 -0700

    Simplify entry point

--- todo/__main__.py
+++ todo/__main__.py
@@ -10,32 +10,19 @@ import todo.resources as _  # compiled resources
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
-
-    match platform.system():
-        case "Darwin":
-            graphics_api = QSGRendererInterface.Metal
-        case "Windows":
-            graphics_api = QSGRendererInterface.OpenGL  # Direct3D11 has issues
-        case _:
-            graphics_api = QSGRendererInterface.OpenGL
-
-    QQuickWindow.setSceneGraphBackend("rhi")
-    QQuickWindow.setGraphicsApi(graphics_api)
-
     app.setOrganizationName("HereIsKevin")
-    app.setOrganizationDomain("hereiskevin.dev")
     app.setApplicationName("Todo")
     app.setWindowIcon(QIcon(":/icons/todo.png"))
 
-    engine = QQmlApplicationEngine()
-
-    QQuickStyle.setStyle("Material")
+    if platform.system() == "Windows":  # Direct3D11 has issues on Windows?
+        QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGL)
 
     QFontDatabase.addApplicationFont(":/fonts/Roboto-Bold.ttf")
     QFontDatabase.addApplicationFont(":/fonts/Roboto-Medium.ttf")
     QFontDatabase.addApplicationFont(":/fonts/Roboto-Regular.ttf")
+    QQuickStyle.setStyle("Material")
 
-    engine.load("qrc:/qml/Todo.qml")
+    engine = QQmlApplicationEngine("qrc:/qml/Todo.qml")
 
     if not engine.rootObjects():
         sys.exit(-1)
