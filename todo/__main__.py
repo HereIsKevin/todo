8a9adaee82da8a2abecdfee1ee9cb3e9bb0409e2
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-06-05 23:42:10 -0700

    Switch to PyQt6 from PySide6

--- todo/__main__.py
+++ todo/__main__.py
@@ -2,10 +2,9 @@ import platform
 import sys
 from pathlib import Path
 
-from PySide6.QtGui import QFontDatabase, QGuiApplication, QIcon
-from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
-from PySide6.QtQuickControls2 import QQuickStyle
+from PyQt6.QtGui import QFontDatabase, QGuiApplication, QIcon
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtQuick import QQuickWindow, QSGRendererInterface
 
 if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
     DATA = Path(sys._MEIPASS).resolve() / "resources"  # type: ignore
@@ -13,13 +12,13 @@ else:
     DATA = Path(__file__).resolve().parent.parent / "resources"
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
+    app = QGuiApplication([*sys.argv, "--style", "Material"])
     app.setOrganizationName("HereIsKevin")
     app.setApplicationName("Todo")
     app.setWindowIcon(QIcon(str(DATA / "icons" / "todo.png")))
 
     if platform.system() == "Windows":  # Direct3D11 has issues on Windows?
-        QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGL)
+        QQuickWindow.setGraphicsApi(QSGRendererInterface.GraphicsApi.OpenGL)
 
     QFontDatabase.addApplicationFont(str(DATA / "fonts" / "Roboto-Bold.ttf"))
     QFontDatabase.addApplicationFont(str(DATA / "fonts" / "Roboto-Medium.ttf"))
