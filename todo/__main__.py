0f380918a575ebf95e8bc2b1155e57dd0156601c
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2022-11-25 00:46:04 -0800

    Remove platform hacks

--- todo/__main__.py
+++ todo/__main__.py
@@ -1,9 +1,7 @@
-import platform
 import sys
 
 from PySide6.QtGui import QFontDatabase, QGuiApplication, QIcon
 from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
 from PySide6.QtQuickControls2 import QQuickStyle
 
 import todo.resources as _  # compiled resources
@@ -13,9 +11,6 @@ if __name__ == "__main__":
     app.setOrganizationName("HereIsKevin")
     app.setApplicationName("Todo")
     app.setWindowIcon(QIcon(":/icons/todo.png"))
-
-    if platform.system() == "Windows":  # Direct3D11 has issues on Windows?
-        QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGL)
 
     QFontDatabase.addApplicationFont(":/fonts/Roboto-Bold.ttf")
     QFontDatabase.addApplicationFont(":/fonts/Roboto-Medium.ttf")
